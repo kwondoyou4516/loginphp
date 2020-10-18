1eb2522b7ab573f1b117a8bfc34a5be4567c2356
kwondoyou4516 <72203141+kwondoyou4516@users.noreply.github.com>
2020-10-18 23:10:52 +0900

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
- <?php
-  session_start();
-  $db = new mysqli("localhost","root","1234","0811member");
-  $db->set_charset("utf8");
-  function mq($sql){
-    global $db;
-    return $db->query($sql);
-  }
-?>
